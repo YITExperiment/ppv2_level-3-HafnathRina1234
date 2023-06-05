8b41cbde715f47fc1e80d780640b4372073320cf
HafnathRina1234 <135530718+HafnathRina1234@users.noreply.github.com>
2023-06-05 07:17:18 +0530

    Add files via upload

---
+++ weight @ height.py
@@ -0,0 +1,16 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+==== RESTART: C:/Users/Singer/AppData/Local/Programs/Python/Python311/age.py ===
+Hello,World!
+what is your name?Rina
+Hello Rina
+>>> age=12
+>>> print(age)
+12
+>>> weight=37
+>>> print(weight)
+37
+>>> height=158
+>>> print(height)
+158
